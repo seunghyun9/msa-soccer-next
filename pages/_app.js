39f80af5ba86f1094440c6a5d1189d8e5786c893
seunghyun <suji1541@gmail.com>
2022-04-12 17:54:35 +0900

    2022 04 12 17:55 join폼 수정

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,30 @@
 import {Layout} from "common";
-import {wrapper} from '../redux/store'
-
-const MyApp = ({ Component, pageProps})=> {
-  return (
+import wrapper from '../redux/store'
+import { PropTypes } from "prop-types"
+import Head from 'next/head'
+const App = ({ Component, pageProps }) => {
+  return (<>
+    <Head>
+      <meta charset="utf-8"/>
+      <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"></meta>
+      <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+      <title>Soccer App</title>
+    </Head>
     <Layout>
       <Component {...pageProps} />
     </Layout>
-  )
+  </>)
+App.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  pageProps: PropTypes.any.isRequired
+  }
 }
-export default wrapper.withRedux(MyApp)
+export default wrapper.withRedux(App)
+  /** 
+  App.propTypes = {
+    Component: PropTypes.elementType.isRequired, // elementType = JSX , 엘리먼트 타입으로 작성하라 
+    pageProps: PropTypes.any.isRequired //any << element만 아니면 된다. 자바스크립트의 JSON은 {},[] 둘다 사용 할수 있지만. 
+  }
+}
+export default wrapper.withRedux(MyApp) // 앱을 리덕스로 감싼다. 데이터 전송에 필요한코드
+*/
--- redux/store.js
+++ redux/store.js
@@ -4,18 +4,20 @@ import {rootSaga} from './sagas'
 import rootReducer from './reducers'
 import { createWrapper} from "next-redux-wrapper";
 
+const isDev = process.env.NODE_ENV !== 'development'
+const idProd =process.env.NODE_ENV !== 'production'
+
 const SagaMiddleware = createSagaMiddleware()
 
 export const createStore = () =>{
     const store = configureStore({
         reducer: rootReducer,
-        devTools: true,
+        devTools: isDev,
         middleware: [ SagaMiddleware]
     })
     SagaMiddleware.run(rootSaga)
     return store
 }
 
-export const wrapper = createWrapper(createStore,
-    {debug: process.env.NODE_ENV !== 'production'}
-    )
+export const wrapper = createWrapper(createStore,{debug: isDev})
+export default wrapper // 자주 가는 통로이기에 속도를 빠르게 하기 위해서 추가
